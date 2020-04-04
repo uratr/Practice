f6700ff594aa46f152280da8206f8a4f83b9d2fa
uratr <urur0303@gmail.com>
2020-04-04 17:58:51 +0900

    関数の内容をわかりやすく変更

--- practice1.m
+++ practice1.m
@@ -12,7 +12,7 @@ EULER_ANGLE_DEG = [40 30 60];
 EulerAngleRad = DegToRad(EULER_ANGLE_DEG);
 
 % DCM
-Q = DirectionCosMat(EulerAngleRad(1), EulerAngleRad(2), EulerAngleRad(3));
+Q = GeoToPer(EulerAngleRad(1), EulerAngleRad(2), EulerAngleRad(3));
 disp(Q)
 
 %%
@@ -20,8 +20,8 @@ function rad = DegToRad(deg)
 rad = deg / 180 * pi;
 end
 %%
-function Qmat = DirectionCosMat(OMEGA,incli,omega)
-Qmat = [ -sin(OMEGA) * cos(incli) * sin(omega) + cos(OMEGA) * cos(omega)...
+function Qgp = GeoToPer(OMEGA,incli,omega)
+Qgp = [ -sin(OMEGA) * cos(incli) * sin(omega) + cos(OMEGA) * cos(omega)...
           cos(OMEGA) * cos(incli) * sin(omega) + sin(OMEGA) * cos(omega)...
           sin(incli) * sin(omega);
          -sin(OMEGA) * cos(incli) * cos(omega) - cos(OMEGA) * sin(omega)...
