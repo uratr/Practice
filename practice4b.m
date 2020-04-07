31f9d9e568011677fd5e20d48281f2521c2e9aa2
uratr <urur0303@gmail.com>
2020-04-07 13:56:06 +0900

    書式統一

--- practice4b.m
+++ practice4b.m
@@ -14,7 +14,18 @@ distance = norm(POSITION_KM);
 energy   = dot(VELOCITY_KMS, VELOCITY_KMS) / 2 - MU_KM3S2 / distance;
 hVector = cross(POSITION_KM, VELOCITY_KMS);
 hNorm = norm(hVector);
-WVector = hVector / hNorm;
-semilatusRectum = hNorm ^ 2 / MU_KM3S2;
+WUnitVector = hVector / hNorm;
+semiLatusRectum = hNorm ^ 2 / MU_KM3S2;
 
-PVector = - MU_KM3S2 / distance * POSITION_KM - cross(hVector, VELOCITY_KMS); 
+PVector = - MU_KM3S2 / distance * POSITION_KM - cross(hVector, VELOCITY_KMS);
+PNorm = norm(PVector);
+PUnitVector = PVector / PNorm;
+QUnitVector = cross(WUnitVector, PUnitVector);
+
+semiMajorAxis = - MU_KM3S2 / (2 * energy);
+eccentricity = PNorm / MU_KM3S2;
+
+if energy < 0
+    ecosE0 = 1 - distance / semiMajorAxis;
+    esinE0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * semiMajorAxis);
+    
