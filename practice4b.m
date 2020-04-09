1ec5ff54b1e56bf18780418a7431b738cd05a86a
uratr <urur0303@gmail.com>
2020-04-09 20:45:38 +0900

    調整

--- practice4b.m
+++ practice4b.m
@@ -7,6 +7,7 @@ clear;
 POSITION_KM  = [ -3670 -3870 4400];
 VELOCITY_KMS = [   4.7  -7.4    1];
 DELTA_TIME_S = 96 * 3600;
+t0 = 0;
 % Constant
 MU_KM3S2 = 398600;
 
@@ -15,7 +16,7 @@ energy   = dot(VELOCITY_KMS, VELOCITY_KMS) / 2 - MU_KM3S2 / distance;
 
 hVector  = cross(POSITION_KM, VELOCITY_KMS);
 hNorm    = norm(hVector);
-PVector  = - MU_KM3S2 / distance * POSITION_KM - cross(hVector, VELOCITY_KMS);
+PVector  = (-MU_KM3S2 / distance) * POSITION_KM - cross(hVector, VELOCITY_KMS);
 PNorm    = norm(PVector);
 
 WUnitVector = hVector / hNorm;
@@ -24,7 +25,7 @@ QUnitVector = cross(WUnitVector, PUnitVector);
 
 semiMajorAxis   = - MU_KM3S2 / (2 * energy);
 eccentricity    = PNorm / MU_KM3S2;
-semiLatusRectum = hNorm ^ 2 / MU_KM3S2;
+semiLatusRectum = (hNorm ^ 2) / MU_KM3S2;
 
 period = 2 * pi / sqrt(MU_KM3S2) * semiMajorAxis ^ 1.5;
 meanMotion = 2 * pi / period;
@@ -32,7 +33,6 @@ if energy < 0
     ecosz0 = 1 - distance / semiMajorAxis;
     esinz0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * semiMajorAxis);
     z0 = atan(esinz0 / ecosz0);
-    t0 = (z0 - eccentricity * sin(z0)) / meanMotion;
     t_pi = t0 - sqrt(semiMajorAxis ^ 3 / MU_KM3S2) * (z0 - esinz0);
 elseif energy > 0
     esinhz0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * (-semiMajorAxis));
@@ -42,30 +42,32 @@ else
     z0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * semiLatusRectum);
     t_pi = t0 - 0.5 * sqrt(semiLatusRectum ^ 3 / MU_KM3S2) * (z0 + z0 ^ 3 / 3);
 end
-z = calcKepler(MU_KM3S2, semiMajorAxis, eccentricity, semiLatusRectum, energy, z0, DELTA_TIME_S, t_pi);
+z = calcKepler(MU_KM3S2, semiMajorAxis, eccentricity, semiLatusRectum, z0, DELTA_TIME_S, t_pi);
+
+fprintf("%d,   %d,   %d\n",z,semiMajorAxis,eccentricity);
 stateVector = KeptoState(MU_KM3S2, semiMajorAxis, eccentricity, semiLatusRectum, energy, PUnitVector, QUnitVector, z);
 fprintf("  %.0f\n  %.0f\n %.0f\n%.3f\n %.3f\n%.4f\n",stateVector);
 %%
 % Kepler Eq
-function zipp = calcKepler(mu, a, e, p, energy, z0, t, t_pi)
+function zipp = calcKepler(mu, a, e, p, z0, t, t_pi)
 nKepler = 100;  % number of iteration of Kepler equation
 TOLERANCE = 1e-8;
 zipp = z0;
 for iKepler = 1:nKepler
-    z = zipp;
-    if energy < 0
-        fz    = z - e * sin(z) - sqrt(mu / a ^ 3) * (t - t_pi);
-        fzdot = 1 - e * cos(z);
-    elseif energy > 0
-        fz    = e * sinh(z) - z - sqrt(mu / (- a) ^ 3) * (t - t_pi);
-        fzdot = e * cosh(z) - 1;
+    zi = zipp;
+    if e < 1
+        fz    = zi - e * sin(zi) - sqrt(mu / (a ^ 3)) * (t - t_pi);
+        fzdot = 1 - e * cos(zi);
+    elseif e > 1
+        fz    = e * sinh(zi) - zi - sqrt(mu / (- a) ^ 3) * (t - t_pi);
+        fzdot = e * cosh(zi) - 1;
     else
-        fz    = z + z ^ 3 / 3 - 2 * sqrt(mu / p ^ 3) * (t - t_pi);
-        fzdot = 1 + z ^ 2;
+        fz    = zi + zi ^ 3 / 3 - 2 * sqrt(mu / p ^ 3) * (t - t_pi);
+        fzdot = 1 + zi ^ 2;
     end
-    zipp = z - fz / fzdot;
-    if abs(zipp) > TOLERANCE
-        continue
+    ratioi = fz / fzdot;
+    if abs(ratioi) > TOLERANCE
+        zipp = zi - ratioi;
     else
         break
     end
