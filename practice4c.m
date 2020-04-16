e22a8f29cd62954a55ff1d249c85db1eccb3236e
uratr <urur0303@gmail.com>
2020-04-16 17:02:40 +0900

    復数の計算を関数化

--- practice4c.m
+++ practice4c.m
@@ -1,8 +1,9 @@
 %%
-% practice 4b
+% practice 4c
 % Initial value > State vector
 % Reference:
-% îºógèróY, "É~ÉbÉVÉáÉìâêÕÇ∆ãOìπê›åvÇÃäÓëb", pp.
+% îºógèróY, "É~ÉbÉVÉáÉìâêÕÇ∆ãOìπê›åvÇÃäÓëb", pp.74-76.
+% universal variable
 %%
 clear;
 % Initial Value
@@ -13,133 +14,78 @@ t0 = 0;
 % Constant
 MU_KM3S2 = 398600;
 
+stateVector = method3(POSITION_KM, VELOCITY_KMS, DELTA_TIME_S, t0, MU_KM3S2);
 fprintf("  %.0f\n  %.0f\n %.0f\n%.3f\n %.3f\n%.4f\n",stateVector);
+%%
+function stateVector = method3(POSITION_KM, VELOCITY_KMS, DELTA_TIME_S, t0, MU_KM3S2)
 %Calculation
-elements = cartesianToKeplerian(POSITION_KM,VELOCITY_KMS,MU_KM3S2);
 distance = norm(POSITION_KM);
-energy   = dot(VELOCITY_KMS, VELOCITY_KMS) / 2 - MU_KM3S2 / distance;
-
-hVector  = cross(POSITION_KM, VELOCITY_KMS);
-hNorm    = norm(hVector);
-PVector  = (-MU_KM3S2 / distance) * POSITION_KM - cross(hVector, VELOCITY_KMS);
-PNorm    = norm(PVector);
-
-WUnitVector = hVector / hNorm;
-PUnitVector = PVector / PNorm;
-QUnitVector = cross(WUnitVector, PUnitVector);
-
-semiMajorAxis   = - MU_KM3S2 / (2 * energy);
-eccentricity    = PNorm / MU_KM3S2;
-semiLatusRectum = (hNorm ^ 2) / MU_KM3S2;
-
-period = 2 * pi / sqrt(MU_KM3S2) * semiMajorAxis ^ 1.5;
-meanMotion = 2 * pi / period;
-if energy < 0
-    ecosz0 = 1 - distance / semiMajorAxis;
-    esinz0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * semiMajorAxis);
-    z0 = atan(esinz0 / ecosz0);
-    t_pi = t0 - sqrt(semiMajorAxis ^ 3 / MU_KM3S2) * (z0 - esinz0);
-elseif energy > 0
-    esinhz0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * (-semiMajorAxis));
-    z0 = asinh(dot(POSITION_KM, VELOCITY_KMS) / (eccentricity * sqrt(MU_KM3S2 * (-semiMajorAxis))));
-    t_pi = t0 - sqrt((-semiMajorAxis) ^ 3 / MU_KM3S2) * (esinhz0 - z0);
+alpha0   = 2 / distance - dot(VELOCITY_KMS, VELOCITY_KMS) / MU_KM3S2;
+if alpha0 > 0
+    x0 = alpha0 * sqrt(MU_KM3S2) * (DELTA_TIME_S - t0);
+elseif alpha0 < 0
+    x0 = 1 / sqrt(-alpha0) * log((2 * MU_KM3S2 *(sqrt(-alpha0)) ^ 3 * (DELTA_TIME_S - t0)) / ...
+        (sqrt(MU_KM3S2) * (1 - alpha0 * distance) + sqrt(-alpha0) * dot(POSITION_KM, VELOCITY_KMS)));
 else
-    z0 = dot(POSITION_KM, VELOCITY_KMS) / sqrt(MU_KM3S2 * semiLatusRectum);
-    t_pi = t0 - 0.5 * sqrt(semiLatusRectum ^ 3 / MU_KM3S2) * (z0 + z0 ^ 3 / 3);
+    fprintf("ERROR\n");
 end
-z = calcKepler(MU_KM3S2, semiMajorAxis, eccentricity, semiLatusRectum, z0, DELTA_TIME_S, t_pi);
-
-stateVector = KeptoState(MU_KM3S2, semiMajorAxis, eccentricity, semiLatusRectum, energy, PUnitVector, QUnitVector, z);
-
-%%
-%Cartesian > Keplerian
-function orbitalElementsRad=cartesianToKeplerian(R, V, MU)
-K         = [0 0 1];
-% Calculation
-distance  = norm(R);
-radialV   = dot(R, V) / distance;
-
-% h: angular momentum
-hVector   = cross(R, V);
-hNorm     = norm(hVector);
-
-% i: inclination
-incliRad = acos(hVector(3) / hNorm);
-
-% OMEGA: right ascension of the ascending node
-NVector   = cross(K, hVector);
-OMEGARad = calcOMEGA(NVector);
-
-% e: eccentricity
-eVector   = 1 / MU * (cross(V, hVector) - MU * R / distance);
-eNorm     = norm(eVector);
-
-% omega: argument of perigee
-omegaRad = calcomethe(NVector, eVector, eVector(3));
-
-% theta: true anomaly
-thetaRad = calcomethe(eVector, R, radialV);
-orbitalElementsRad = [hNorm, incliRad, OMEGARad, eNorm, omegaRad, thetaRad];
+x = calc_x(DELTA_TIME_S,t0,POSITION_KM,VELOCITY_KMS,alpha0,x0,MU_KM3S2);
+stateVector = LagtoState(MU_KM3S2, POSITION_KM, VELOCITY_KMS, DELTA_TIME_S, t0, x, alpha0);
     %%
-    % Calculate OMEGA
-    function OMEGA = calcOMEGA(N)
-    if N(2) >= 0
-        OMEGA = acos(N(1) / norm(N));
-    else
-        OMEGA = 2 * pi - acos(N(1) / norm(N));
+    % calculate position & velocity
+    function stateVector = LagtoState(mu, R0, V0, t, t0, x, alpha0)
+    r0 = norm(R0);
+    f = 1 - (x ^ 2) / r0 * funC(alpha0 * (x ^ 2));
+    g = t - t0 - (x ^ 3) / sqrt(mu) * funS(alpha0 * (x ^ 2));
+    R = f * R0 + g * V0;
+    r = norm(R);
+    fd = sqrt(mu) / (r * r0) * (alpha0 * (x ^ 3) * funS(alpha0 * (x ^ 2)) - x);
+    gd = 1 - (x ^ 2) / r * funC(alpha0 * (x ^ 2));
+    V = fd * R0 + gd * V0;
+    stateVector = [R, V];
     end
-    end
-    %%
-    % calculate omega & theta
-    function angle = calcomethe(A, B, c)
-    if c >= 0
-        angle = acos(dot(A / norm(A), B / norm(B)));
-    else
-        angle = 2 * pi - acos(dot(A / norm(A), B / norm(B)));
-    end
-    end
-end
 %%
-% Kepler Eq
-function zipp = calcKepler(mu, a, e, p, z0, t, t_pi)
-nKepler = 100;  % number of iteration of Kepler equation
-TOLERANCE = 1e-8;
-zipp = z0;
-for iKepler = 1:nKepler
-    zi = zipp;
-    if e < 1
-        fz    = zi - e * sin(zi) - sqrt(mu / (a ^ 3)) * (t - t_pi);
-        fzdot = 1 - e * cos(zi);
-    elseif e > 1
-        fz    = e * sinh(zi) - zi - sqrt(mu / (- a) ^ 3) * (t - t_pi);
-        fzdot = e * cosh(zi) - 1;
-    else
-        fz    = zi + zi ^ 3 / 3 - 2 * sqrt(mu / p ^ 3) * (t - t_pi);
-        fzdot = 1 + zi ^ 2;
+    function result = calc_x(t,t0,R0,V0,alpha0,x0,mu)
+        r0   = norm(R0);
+        xp    = x0;
+        for ix = 1:100
+            z    = alpha0 * (xp ^ 2);
+            S_z  = funS(z);
+            C_z  = funC(z);
+            F_x  = (1 - alpha0 * r0) * (xp ^ 3) * S_z +...
+                   dot(R0, V0) / sqrt(mu) * (xp ^ 2) * C_z +...
+                   r0 * xp - sqrt(mu) * (t - t0);
+            dFdx = (1 - alpha0 * r0) * (xp ^ 2) * C_z +...
+                   dot(R0, V0) / sqrt(mu) * xp * (1 - z * S_z) + r0;
+            xpp  = xp - F_x / dFdx;
+            if (abs (xpp - xp) > 1e-8) && (ix < 100)
+                xp = xpp;
+            elseif (abs (xpp - xp) > 1e-8) && (ix >= 100)
+                fprintf("ERROR_x\n");
+            else
+                break
+            end
+        end
+        result = xp;
     end
-    ratioi = fz / fzdot;
-    if abs(ratioi) > TOLERANCE
-        zipp = zi - ratioi;
-    else
-        break
-    end
-end
-end
 %%
-% calculate position & velocity
-function stateVector = KeptoState(mu, a, e, p, energy, P, Q, z)
-if energy < 0
-    R = a * (cos(z) - e) * P + sqrt(a * p) * sin(z) * Q;
-    r = norm(R);
-    V = - sqrt(mu * a) / r * sin(z) * P + sqrt(mu * p) / r * cos(z) * Q;
-elseif energy > 0
-    R = - a * (e - cosh(z)) * P + sqrt(- a * p) * sinh(z) * Q;
-    r = norm(R);
-    V = - sqrt(mu * (- a)) / r * sinh(z) * P + sqrt(mu * p) / r * cosh(z) * Q;
-else
-    R = p / 2 * (1 - z ^ 2) * P + p * z * Q;
-    r = norm(R);
-    V = - sqrt(mu * p) / r * z * Q + sqrt(mu * p) / r * Q;
-end
-stateVector = [R, V];
+    function result = funS(x)
+        if x > 0
+            result = (sqrt(x) - sin(sqrt(x))) / ((sqrt(x)) ^ 3);
+        elseif x < 0
+            result = (sinh(sqrt(-x)) - sqrt(-x)) / ((sqrt(-x)) ^ 3);
+        else
+            result  = 1 / factorial(3);
+        end
+    end
+%%
+    function result = funC(x)
+        if x > 0
+            result = (1 - cos(sqrt(x))) / x;
+        elseif x < 0
+            result = (cosh(sqrt(-x)) - 1) / (-x);
+        else
+            result  = 1 / factorial(2);
+        end
+    end
 end
